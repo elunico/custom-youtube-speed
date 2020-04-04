22682b18a21e629bc59416dfb461418f53279805
Thomas Povinelli <tompov227@gmail.com>
2020-04-04 15:07:18 -0400

    Added preset buttons

--- ext/src/browser_action/browser_action.js
+++ ext/src/browser_action/browser_action.js
@@ -1,4 +1,5 @@
 let element = document.getElementById('cys-speedRange');
+let presetButtons = document.getElementsByClassName('btn-preset');
 
 chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
   chrome.tabs.sendMessage(
@@ -16,6 +17,26 @@ chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
       });
 });
 
+for (let button of presetButtons) {
+  button.onclick = () => {
+    let btnValue = Number(button.value.substring(0, button.value.length - 1));
+    let d = document.getElementById('cys-speedDiv');
+    d.innerHTML = button.value;
+    element.value = btnValue;
+    chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+      chrome.tabs.sendMessage(
+          tabs[0].id, {from: 'cys', message: 'speed-change', speed: btnValue},
+          function(response) {
+            if (!response) {
+              console.error('No Response');
+            }
+            if (!response.ok) {
+              console.error(`Invalid Response! ${response.reason}`);
+            }
+          })
+    });
+  }
+}
 
 element.oninput = () => {
   let d = document.getElementById('cys-speedDiv');
