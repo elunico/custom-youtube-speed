c49ba261ad3d53b65dad9dc96c4974acafc7f75f
thomaspovinelli <tompov227@gmail.com>
2020-04-23 15:40:34 -0400

    Made edit a toggle button like spacebar

--- ext/src/browser_action/browser_action.js
+++ ext/src/browser_action/browser_action.js
@@ -1,6 +1,6 @@
 let element = document.getElementById('cys-speedRange');
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.from = 'cys' && request.message == 'options-loaded') {
     let speed = Number(request.speed);
     element.value = speed;
@@ -8,7 +8,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     d.innerHTML = `${speed}`;
     console.log(`UI updated for speed ${speed}`);
     sendResponse(
-        {ok: true, reason: `Updated UI for speed ${speed}`, speed: speed});
+      { ok: true, reason: `Updated UI for speed ${speed}`, speed: speed });
   }
 });
 
@@ -16,34 +16,34 @@ let presetButtons = document.getElementsByClassName('btn-preset');
 let saveButton = document.getElementById('btn-save-speed');
 
 saveButton.onclick = () => {
-  chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     chrome.tabs.sendMessage(
-        tabs[0].id, {from: 'cys', message: 'speed-save'}, function(response) {
-          let d = document.getElementById('save-status');
-          if (response && response.ok) {
-            d.innerHTML = `Saved default speed as ${response.speed}`;
-          } else {
-            d.innerHTML = 'Error could not save!';
-          }
-          setTimeout(() => {d.innerHTML = ''}, 800);
-        });
+      tabs[0].id, { from: 'cys', message: 'speed-save' }, function (response) {
+        let d = document.getElementById('save-status');
+        if (response && response.ok) {
+          d.innerHTML = `Saved default speed as ${response.speed}`;
+        } else {
+          d.innerHTML = 'Error could not save!';
+        }
+        setTimeout(() => { d.innerHTML = '' }, 800);
+      });
   });
 };
 
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
   chrome.tabs.sendMessage(
-      tabs[0].id, {from: 'cys', message: 'speed-query'}, function(response) {
-        let d = document.getElementById('cys-speedDiv');
-        if (!response || !response.ok) {
-          element.value = 0;
-          d.innerHTML = '&lt;No Video&gt;';
-          element.setAttribute('disabled', 'true');
-        } else {
-          element.removeAttribute('disabled');
-          element.value = Number(response['current-speed'] || 1);
-          d.innerHTML = Number(response['current-speed'] || 1);
-        }
-      });
+    tabs[0].id, { from: 'cys', message: 'speed-query' }, function (response) {
+      let d = document.getElementById('cys-speedDiv');
+      if (!response || !response.ok) {
+        element.value = 0;
+        d.innerHTML = '&lt;No Video&gt;';
+        element.setAttribute('disabled', 'true');
+      } else {
+        element.removeAttribute('disabled');
+        element.value = Number(response['current-speed'] || 1);
+        d.innerHTML = Number(response['current-speed'] || 1);
+      }
+    });
 });
 
 for (let button of presetButtons) {
@@ -52,19 +52,19 @@ for (let button of presetButtons) {
     let d = document.getElementById('cys-speedDiv');
     d.innerHTML = button.value;
     element.value = btnValue;
-    chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+    chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
       chrome.tabs.sendMessage(
-          tabs[0].id, {from: 'cys', message: 'speed-change', speed: btnValue},
-          function(response) {
-            if (!response) {
-              console.error('No Response');
-              return;
-            }
-            if (!response.ok) {
-              console.error(`Invalid Response! ${response.reason}`);
-              return;
-            }
-          });
+        tabs[0].id, { from: 'cys', message: 'speed-change', speed: btnValue },
+        function (response) {
+          if (!response) {
+            console.error('No Response');
+            return;
+          }
+          if (!response.ok) {
+            console.error(`Invalid Response! ${response.reason}`);
+            return;
+          }
+        });
     });
   }
 }
@@ -73,19 +73,19 @@ element.oninput = () => {
   let d = document.getElementById('cys-speedDiv');
   d.innerHTML = element.value;
 
-  chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     chrome.tabs.sendMessage(
-        tabs[0].id,
-        {from: 'cys', message: 'speed-change', speed: element.value},
-        function(response) {
-          if (!response) {
-            console.error('No response');
-            return;
-          }
-          if (!response.ok) {
-            console.error(`Invalid Response! ${response.reason}`);
-            return;
-          }
-        });
+      tabs[0].id,
+      { from: 'cys', message: 'speed-change', speed: element.value },
+      function (response) {
+        if (!response) {
+          console.error('No response');
+          return;
+        }
+        if (!response.ok) {
+          console.error(`Invalid Response! ${response.reason}`);
+          return;
+        }
+      });
   });
 };
--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,25 +1,33 @@
 function loadDefault() {
-  chrome.storage.sync.get({'cys-default-speed': 1}, function(items) {
+  chrome.storage.sync.get({ 'cys-default-speed': 1 }, function (items) {
     let speed = Number(items['cys-default-speed']);
     let video = document.getElementsByTagName('video')[0];
     if (video) {
       video.playbackRate = speed;
       console.log(`Set speed to loaded default: ${speed}`);
       chrome.runtime.sendMessage(
-          {message: 'options-loaded', from: 'cys', speed: speed},
-          function(response) {
-            if (response && response.ok) {
-              console.log('UI Updated for loaded options');
-            } else {
-              console.error(response);
-            }
-          });
+        { message: 'options-loaded', from: 'cys', speed: speed },
+        function (response) {
+          if (response && response.ok) {
+            console.log('UI Updated for loaded options');
+          } else {
+            console.error(response);
+          }
+        });
     }
   });
 }
 
+function togglePause(videoElt) {
+  if (videoElt.paused) {
+    videoElt.play();
+  } else {
+    videoElt.pause();
+  }
+}
+
 function setHandler() {
-  document.addEventListener('keypress', function(event) {
+  document.addEventListener('keypress', function (event) {
     let video = document.getElementsByTagName('video')[0];
     console.log(event);
     console.log(event.key);
@@ -29,48 +37,48 @@ function setHandler() {
       } else if (event.key == '[') {
         video.currentTime -= (10 * video.playbackRate);
       } else if (event.key == 'Enter') {
-        video.pause();
+        togglePause(video);
       }
     }
   });
 }
 
-chrome.extension.sendMessage({}, function(response) {
-  var readyStateCheckInterval = setInterval(function() {
+chrome.extension.sendMessage({}, function (response) {
+  var readyStateCheckInterval = setInterval(function () {
     if (document.readyState === 'complete') {
       clearInterval(readyStateCheckInterval);
       loadDefault();
 
       setHandler();
 
-      chrome.runtime.onMessage.addListener(function(
-          request, sender, sendRepsonse) {
+      chrome.runtime.onMessage.addListener(function (
+        request, sender, sendRepsonse) {
         if (request.from === 'cys' && request.message === 'speed-change') {
           let speed = Number(request.speed);
           let video = document.getElementsByTagName('video')[0];
           if (!video) {
-            sendRepsonse({ok: false, reason: 'Video element not found'});
+            sendRepsonse({ ok: false, reason: 'Video element not found' });
           } else {
             video.playbackRate = speed;
-            sendRepsonse({ok: true});
+            sendRepsonse({ ok: true });
           }
         }
         if (request.from === 'cys' && request.message === 'speed-query') {
           let video = document.getElementsByTagName('video')[0];
           if (video) {
-            sendRepsonse({ok: true, 'current-speed': video.playbackRate});
+            sendRepsonse({ ok: true, 'current-speed': video.playbackRate });
           } else {
-            sendRepsonse({ok: false, reason: 'No video element found'});
+            sendRepsonse({ ok: false, reason: 'No video element found' });
           }
         }
         if (request.from === 'cys' && request.message === 'speed-save') {
           let speed = document.getElementsByTagName('video')[0].playbackRate;
           console.log('Attempting save');
           chrome.storage.sync.set(
-              {'cys-default-speed': Number(speed)}, function() {
-                console.log('Sending response');
-                sendRepsonse({ok: true, speed: speed});
-              });
+            { 'cys-default-speed': Number(speed) }, function () {
+              console.log('Sending response');
+              sendRepsonse({ ok: true, speed: speed });
+            });
           return true;
         }
       });
