0015f039a5127b3d64445f1f0c33170a8d09a848
Tom <elunico@users.noreply.github.com>
2022-08-17 09:53:20 -0400

    Fixed a bug where the save button in options on firefox did not work. Resolves #15

--- firefox/custom-youtube-speed/src/options/options.js
+++ firefox/custom-youtube-speed/src/options/options.js
@@ -147,3 +147,7 @@ function restore_options() {
     }
   });
 }
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click',
+  save_options);
